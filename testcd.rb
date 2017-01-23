aee96d8778e064fb2849f328b4a4881273163860
Nope <local@host>
2017-01-23 19:53:39 +0000

    adding testcd.rb

---
+++ testcd.rb
@@ -0,0 +1,6 @@
+# All variables must start with "var_" to help identify variables. Constants start with "Cons_"
+var_number1 = 55
+var_number2 = 3
+Cons_number3 = 888
+total = var_number1 + var_number2
+puts total
