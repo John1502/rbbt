d842f9cf0f6458535f72d1178d1af0419634f61e
Nope <local@host>
2017-01-30 19:30:25 +0000

    Added ruby test which seems to have issue when using the .next method with the letter z

---
+++ rubytestnextmethod.rb
@@ -0,0 +1,8 @@
+puts "We are testing to see what happens when we end with cz".next
+puts "Versus when we end with any other letter c".next
+puts "How about multiple letters of z at the end of the sentence zz".next
+puts "Are there any others this applies to?".next
+
+
+
+
